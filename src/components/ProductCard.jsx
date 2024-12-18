a2639dd06256c3c864cb9f5fc5401a5b4d1b3a70
AlexiaBarboza <alexiaareli31102001gmail.com>
2024-12-18 10:11:59 -0600

    34

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -37,7 +37,7 @@ function ProductCard({product}) {
                         </Tooltip>
                         <Tooltip content="Actualizar">
                             <button className='bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg text-sm'>
-                                <Link to={'/products/'+product._id}>
+                                <Link to={'/product/'+product._id}>
                                     <IoPencilSharp />
                                 </Link>
                             </button>
